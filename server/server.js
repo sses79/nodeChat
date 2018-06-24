40a53e0de02bb8a43d51210a48002ca2a2a9a01d
tim.mac <sses79@hotmail.com>
2018-06-24 21:42:42 +0100

    init

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+let app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Started at ${port}`);
+});
+
+module.exports = {app};
